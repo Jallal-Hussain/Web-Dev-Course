751a5daf3f0d07f00bbeb46b057cc0be8bc8bd9c
Jalal Hussain <jalal143880@gmail.com>
2024-03-19 23:23:59 +0500

    Setting Endpoints.

--- Backend/server.js
+++ Backend/server.js
@@ -3,41 +3,44 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { login, register } from "./Controller/auth.js";
 
-
-// import uploadRoute from 'cookie-parser'
-
-const app = express()
+const app = express();
 
 dotenv.config();
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
+// Set endpoints
+app.post("/api/auth/register", register); // Assuming register handles POST requests
+app.post("/api/auth/login", login); // Assuming login handles POST requests
 
 const DatabaseConnection = async () => {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI);
-      console.log("Connected to mongoDB.");
-    } catch {
-      console.log("Connection Error");
-    }
-  };
-  
-  mongoose.connection.on("disconnected", () => {
-    console.log("MongoDB Disconnected!");
-  });
-  
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    console.error("Connection Error:", error);
+    process.exit(1); // Exit the process if unable to connect to the database
+  }
+};
 
-app.get("/", (req, res)=>{
-res.json({message:"Hello"})
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB Disconnected!");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error("MongoDB Connection Error:", error);
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello" });
+});
 
 const port = process.env.PORT || 5000;
-
-
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
@@ -49,8 +52,8 @@ app.use((err, req, res, next) => {
     stack: err.stack,
   });
 });
+
 app.listen(port, () => {
-DatabaseConnection();
-  console.log(`Server Listen on port ${port}`);
-  console.log("Connected to backend.");
+  DatabaseConnection();
+  console.log(`Server listening on port ${port}`);
 });
